e7f3d695ca9c5e77848e31cf6657d7a93f9c8de7
Tarun-Kumar07 <tarunkumar00137@gmail.com>
2024-10-24 20:52:10 +0530

    Benchmark number of simulated qubits

---
+++ src/benchmark/benchmark_num_qubits.py
@@ -0,0 +1,20 @@
+from benchmark.benchmark_execution_time import run_quantum_circuit
+from benchmark.dataset_generator import generate_random_circuit
+from simulator import SimulatorType
+
+DEPTH = 10
+MAX_QUBITS = 30
+
+
+def print_execution_time(simulator_type: SimulatorType) -> int:
+    for qubits in range(2, MAX_QUBITS):
+        circuit = generate_random_circuit(qubits, DEPTH)
+        execution_time, _ = run_quantum_circuit(simulator_type, qubits, circuit)
+        print(
+            f" Simulator of type {simulator_type} took {execution_time * (10 ** -6)} for {qubits} qubits"
+        )
+
+
+if __name__ == "__main__":
+    # print_execution_time(SimulatorType.MATRIX)
+    print_execution_time(SimulatorType.TENSOR)
